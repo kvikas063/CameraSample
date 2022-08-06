6682ae884a0ddc164297d7e8b98501cf9c190559
Vikas Kumar <vikas@myhealthcare.life>
2022-08-06 10:19:04 +0530

    Initial Commit

---
+++ iOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAssignment
+//
+//  Created by Vikas Kumar on 06/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
